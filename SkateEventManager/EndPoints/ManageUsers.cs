f262d5dcc9daa04eee1cf1930266ffccf5162e57
margareta <margaretavida120@gmail.com>
2025-04-25 13:58:35 +0200

    all user

--- SkateEventManager/EndPoints/ManageUsers.cs
+++ SkateEventManager/EndPoints/ManageUsers.cs
@@ -1,16 +1,26 @@
-﻿namespace SkateEventManager.EndPoints;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SkateEventManager.EndPoints;
 
 public static class ManageUsers
 {
     public static WebApplication GetUsers(this WebApplication app)
     {
-        app.MapGet("/currentUser/{id}", (int id, DatabaseContext db) =>
+        app.MapGet("/currentUser/{id}", async (int id, DatabaseContext db) =>
         {
             var user = db.User.FirstOrDefault(u => u.Id == id);
 
             return user != null
                 ? Results.Ok(user)
                 : Results.NotFound("User not found.");
+        });
+
+        app.MapGet("/users", async (DatabaseContext db) =>
+        {
+            var users = await db.User.ToListAsync();
+            return users.Any()
+                ? Results.Ok(users)
+                : Results.NotFound("No users found.");
         });
 
         return app;
